dbef37b718621b07d099e74013bf61e2c9bef235
Abbas <a.samadani256@gmail.com>
2021-04-17 01:12:06 +0100

    adding theme to project and install laravel mix

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.index');
+});
+
+Route::get('/admin', function () {
+    return view('back.index');
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,33 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.styles([    
+    'resources/views/front/lib/bootstrap/css/bootstrap.min.css',
+     //'resources/views/front/lib/font-awesome/css/font-awesome.min.css',
+     'resources/views/front/lib/animate/animate.min.css',
+     'resources/views/front/lib/ionicons/css/ionicons.min.css',
+     'resources/views/front/lib/owlcarousel/assets/owl.carousel.min.css',
+     'resources/views/front/lib/lightbox/css/lightbox.min.css',
+     'resources/views/front/css/style.css'
+ ],
+  'public/front/css/app.css'
+  )
+
+ 
+mix.scripts([
+    'resources/views/front/lib/jquery/jquery.min.js',
+    'resources/views/front/lib/jquery/jquery-migrate.min.js',
+    'resources/views/front/lib/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/views/front/lib/easing/easing.min.js',
+    'resources/views/front/lib/mobile-nav/mobile-nav.js',
+    'resources/views/front/lib/wow/wow.min.js',
+    'resources/views/front/lib/waypoints/waypoints.min.js',
+    'resources/views/front/lib/counterup/counterup.min.js',
+    'resources/views/front/lib/owlcarousel/owl.carousel.min.js',
+    'resources/views/front/lib/isotope/isotope.pkgd.min.js',
+    'resources/views/front/contactform/contactform.js',
+    'resources/views/front/js/main.js'
+],
+
+    'public/front/js/app.js'
+);
